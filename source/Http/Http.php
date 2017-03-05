6e69dea7c8034532623864584c762b8385d916d1
Sean Morris <sean@sean-morris.com>
2017-03-05 14:19:45 -0500

    Cleanup.

--- source/Http/Http.php
+++ source/Http/Http.php
@@ -39,25 +39,4 @@ class Http
 
 		static::$disconnect[] = $function;		
 	}
-
-	public static function disconnect($url)
-	{
-		set_time_limit(0);
-		ignore_user_abort(1);
-
-		header(sprintf("Location: %s", $url));
-		header(sprintf("Content-Length: %s", ob_get_length()));
-
-		header('Connection: close');
-
-		\Base\Log::file(ob_get_level());
-
-		ob_flush();
-		ob_end_flush();
-		flush();
-
-		session_write_close();
-
-		\Base\Log::file('Output sent.');
-	}
 }
