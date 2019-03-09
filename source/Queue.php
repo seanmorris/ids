49bb17b49ad3107c73ea75fdd5a9871b0b89753b
Sean Morris <sean@seanmorr.is>
2019-03-09 14:30:44 -0500

    Working Rabbit RPC

--- source/Queue.php
+++ source/Queue.php
@@ -6,28 +6,40 @@ use PhpAmqpLib\Message\AMQPMessage,
 
 abstract class Queue
 {
-	const RABBIT_MQ_SERVER  = 'default'
-		, QUEUE_PASSIVE     = FALSE
-		, QUEUE_DURABLE     = FALSE
-		, QUEUE_EXCLUSIVE   = FALSE
-		, QUEUE_AUTO_DELETE = FALSE
-		, CHANNEL_LOCAL     = FALSE
-		, CHANNEL_NO_ACK    = TRUE
-		, CHANNEL_EXCLUSIVE = FALSE
-		, CHANNEL_WAIT      = FALSE
-		, BATCH_ACKS        = FALSE
+	const RABBIT_MQ_SERVER   = 'default'
+		, QUEUE_PASSIVE      = FALSE
+		, QUEUE_DURABLE      = FALSE
+		, QUEUE_EXCLUSIVE    = FALSE
+		, QUEUE_AUTO_DELETE  = FALSE
+		, CHANNEL_LOCAL      = FALSE
+		, CHANNEL_NO_ACK     = TRUE
+		, CHANNEL_EXCLUSIVE  = FALSE
+		, CHANNEL_WAIT       = FALSE
+		, BATCH_ACKS         = FALSE
 
-		, PREFETCH_COUNT    = 1
-		, PREFETCH_SIZE     = 0
+		, PREFETCH_COUNT     = 1
+		, PREFETCH_SIZE      = 0
 
-		, ASYNC             = FALSE
+		, SEND               = TRUE // not implemented
+		, BROADCACT          = TRUE // not implemented
+		, RPC                = FALSE
 
-		, BROADCAST_QUEUE   = '::broadcast'
-		, TOPIC_QUEUE       = '::topic'
-		, SEND_QUEUE        = '::send'
+		, ASYNC              = FALSE
+
+		, BROADCAST_QUEUE    = '::broadcast'
+		, TOPIC_QUEUE        = '::topic'
+		, SEND_QUEUE         = '::send'
+		, RPC_RESPONSE_QUEUE = '::rpcResponse'
+		, RPC_SEND_QUEUE     = '::rpcSend'
 	;
 
-	protected static $channel, $broadcastQueue, $topicQueues = [];
+	protected static
+		$channel
+		, $broadcastQueue
+		, $rpcSendQueue
+		, $rpcResponseQueue
+		, $topicQueues = []
+		, $rpcDone     = [];
 
 	public static function init(){}
 
@@ -38,7 +50,7 @@ abstract class Queue
 		$channel = static::getChannel();
 
 		$exchange = get_called_class() . static::TOPIC_QUEUE;
-		
+
 		if($topic === NULL)
 		{
 			$exchange = '';
@@ -52,24 +64,42 @@ abstract class Queue
 		);
 	}
 
-	public static function broadcast($message)
+	public static function broadcast($message, $topic = NULL)
 	{
 		$channel = static::getChannel();
 
+		$exchange = get_called_class() . static::TOPIC_QUEUE;
+
+		$exchange = get_called_class() . static::TOPIC_QUEUE;
+
+		if($topic === NULL)
+		{
+			$exchange = get_called_class() . static::BROADCAST_QUEUE;
+			$topic    = NULL;
+		}
+
 		$channel->basic_publish(
 			new AMQPMessage(serialize($message))
-			, get_called_class() . static::BROADCAST_QUEUE
+			, $exchange
+			, $topic
 		);
 	}
 
-	public static function manageReciept($message)
+	public static function manageReciept($message, $callback = NULL)
 	{
-		$result = static::recieve(unserialize($message->body), $message);
+		if($callback)
+		{
+			$result = $callback(unserialize($message->body), $message);
+		}
+		else
+		{
+			$result = static::recieve(unserialize($message->body), $message);
+		}
 
 		if(!static::CHANNEL_NO_ACK)
 		{
 			if($result === FALSE)
-			{	
+			{
 				$message->delivery_info['channel']->basic_nack(
 					$message->delivery_info['delivery_tag']
 					, static::BATCH_ACKS
@@ -100,7 +130,7 @@ abstract class Queue
 		if(!static::$channel || $reset)
 		{
 			$servers = \SeanMorris\Ids\Settings::read('rabbitMq');
-			
+
 			if(!$servers)
 			{
 				throw new \Exception('No RabbitMQ servers specified.');
@@ -143,6 +173,16 @@ abstract class Queue
 					$channel->queue_delete(static::$broadcastQueue);
 				}
 
+				// if(static::$rpcSendQueue)
+				// {
+				// 	$channel->queue_delete(static::$rpcSendQueue);
+				// }
+
+				if(static::$rpcResponseQueue)
+				{
+					$channel->queue_delete(static::$rpcResponseQueue);
+				}
+
 				foreach(static::$topicQueues as $topicQueue)
 				{
 					$channel->queue_delete($topicQueue);
@@ -155,6 +195,115 @@ abstract class Queue
 			static::$channel = $channel;
 		}
 		return static::$channel;
+	}
+
+
+	public static function rpc($message)
+	{
+		$correlationId = uniqid();
+
+		static::$rpcDone[$correlationId] = FALSE;
+
+		$channel = static::getChannel();
+
+		$channel->basic_publish(
+			new AMQPMessage(serialize($message), [
+				'correlation_id' => $correlationId
+				, 'reply_to'     => static::getRpcResponseQueue()
+			])
+			, ''
+			, static::getRpcSendQueue()
+		);
+
+		if(!static::ASYNC)
+		{
+			while (!static::$rpcDone[$correlationId])
+			{
+				$channel->wait();
+			}
+
+			$response = static::$rpcDone[$correlationId];
+
+			unset(static::$rpcDone[$correlationId]);
+
+			return $response->body;
+		}
+		else
+		{
+			return function() use($channel) {
+				$result = null;
+
+				$responseQueueName = static::getRpcResponseQueue();
+
+				while(($response = $channel->basic_get($responseQueueName)) !== NULL)
+				{
+					$result = unserialize($response->body);
+
+					var_dump($result);
+
+					if($result !== FALSE)
+					{
+						$channel->basic_ack($response->delivery_info['delivery_tag']);
+					}
+
+					return $result;
+				}
+			};
+		}
+	}
+
+	protected static function getRpcSendQueue()
+	{
+		if(!static::$rpcSendQueue)
+		{
+			$channel = static::getChannel();
+
+			list(static::$rpcSendQueue, ,) = $channel->queue_declare(
+				get_called_class() . static::RPC_SEND_QUEUE
+			);
+		}
+
+		return static::$rpcSendQueue;
+	}
+
+	protected static function getRpcResponseQueue()
+	{
+		if(!static::$rpcResponseQueue)
+		{
+			$channel = static::getChannel();
+
+			list(static::$rpcResponseQueue, , ) = $channel->queue_declare('');
+
+			if(!static::ASYNC)
+			{
+				$channel->basic_consume(
+					static::$rpcResponseQueue
+					, ''
+					, FALSE
+					, TRUE
+					, FALSE
+					, TRUE
+					, function($response){
+						if(!$correlationId = $response->get('correlation_id'))
+						{
+							return FALSE;
+						}
+
+						if(
+							!isset(static::$rpcDone[$correlationId])
+								|| static::$rpcDone[$correlationId] !== FALSE
+								|| isset(static::$rpcResult[$correlationId])
+						){
+							return FALSE;
+						}
+
+						static::$rpcDone[$correlationId] = $response;
+					}
+				);
+			}
+		}
+
+		return static::$rpcResponseQueue;
 	}
 
 	protected static function getBroadcastQueue()
@@ -221,8 +370,6 @@ abstract class Queue
 
 		if($topic === NULL)
 		{
-			$broadcastQueueName = static::getBroadcastQueue();
-
 			while($message = $channel->basic_get(get_called_class() . static::SEND_QUEUE))
 			{
 				$result = call_user_func_array($callback, [
@@ -235,6 +382,8 @@ abstract class Queue
 					$channel->basic_ack($message->delivery_info['delivery_tag']);
 				}
 			}
+
+			$broadcastQueueName = static::getBroadcastQueue();
 
 			while($message = $channel->basic_get($broadcastQueueName))
 			{
@@ -266,13 +415,42 @@ abstract class Queue
 				}
 			}
 		}
+
+		if(static::RPC)
+		{
+			$sendQueueName = static::getRpcSendQueue();
+
+			while($request = $channel->basic_get($sendQueueName))
+			{
+				$result = call_user_func_array($callback, [
+					unserialize($request->body)
+					, $request
+				]);
+
+				$channel->basic_publish(
+					new AMQPMessage(
+						serialize($result)
+						, ['correlation_id' => $request->get('correlation_id')]
+					)
+					, ''
+					, $request->get('reply_to')
+				);
+
+				if($result !== FALSE)
+				{
+					$channel->basic_ack($request->delivery_info['delivery_tag']);
+				}
+			}
+		}
 	}
 
-	public static function listen($topic = NULL)
+	public static function listen($callback = NULL, $topic = NULL)
 	{
 		static::init();
 
-		$callback = [get_called_class(), 'manageReciept'];
+		$wrapper = function($message) use($callback){
+			return static::manageReciept($message, $callback);
+		};
 
 		if(static::ASYNC)
 		{
@@ -294,7 +472,7 @@ abstract class Queue
 					, static::CHANNEL_NO_ACK
 					, static::CHANNEL_EXCLUSIVE
 					, static::CHANNEL_WAIT
-					, $callback
+					, $wrapper
 				);
 
 				$broadcastQueueName = static::getBroadcastQueue();
@@ -306,7 +484,7 @@ abstract class Queue
 					, static::CHANNEL_NO_ACK
 					, TRUE
 					, static::CHANNEL_WAIT
-					, $callback
+					, $wrapper
 				);
 			}
 			else
@@ -320,7 +498,22 @@ abstract class Queue
 					, static::CHANNEL_NO_ACK
 					, TRUE
 					, static::CHANNEL_WAIT
-					, $callback
+					, $wrapper
+				);
+			}
+
+			if(static::RPC)
+			{
+				$topicQueueName = static::getRpcSendQueue($topic);
+
+				$channel->basic_consume(
+					$topicQueueName
+					, ''
+					, static::CHANNEL_LOCAL
+					, static::CHANNEL_NO_ACK
+					, TRUE
+					, static::CHANNEL_WAIT
+					, $wrapper
 				);
 			}
 
