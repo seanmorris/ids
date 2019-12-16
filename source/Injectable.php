4f7adf2d07d90c094d92fb0538cc241f06220147
Sean Morris <sean@seanmorr.is>
2019-12-16 03:08:49 -0500

    Style.

--- source/Injectable.php
+++ source/Injectable.php
@@ -1,11 +1,9 @@
 <?php
 namespace SeanMorris\Ids;
-trait Loader
+trait Injectable
 {
 	public function __get($name)
 	{
-		define('static::LOL', 1);
-
 		if(!isset(static::$_injections))
 		{
 			return;
