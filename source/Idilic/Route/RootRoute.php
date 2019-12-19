962e3714b7a220d73ef55cae80170fd042e4dea5
Sean Morris <sean@seanmorr.is>
2019-12-19 11:37:34 -0500

    Cleaning up idilic root.

--- source/Idilic/Route/RootRoute.php
+++ source/Idilic/Route/RootRoute.php
@@ -920,93 +920,6 @@ EOF
 		);
 	}
 
-	/** Multiota specific. */
-
-	public function batch($router)
-	{
-		$job = new \SeanMorris\Multiota\Test\Count\CountJob;
-		$job->start();
-		/*
-		$pool = new \SeanMorris\Multiota\Pool(
-			'\SeanMorris\Multiota\DataSource'
-			, '\SeanMorris\Multiota\Processor'
-		);
-
-		$pool->start();
-		*/
-		exit;
-	}
-
-	/** Multiota specific. */
-
-	public function batchProcess($router)
-	{
-
-		$processor = $router->path()->consumeNode();
-		$child = $router->path()->consumeNode();
-		$max = $router->path()->consumeNode();
-		$timeout = $router->path()->consumeNode();
-
-		$processor = new $processor($child, $max, $timeout);
-
-		$processor->spin();
-	}
-
-	/** Multiota specific. */
-
-	public function countJob()
-	{
-		$job = new \SeanMorris\Multiota\Test\Count\CountJob(
-			'SeanMorris\Multiota\RemotePool'
-			, [
-				'servers' => ['localhost']
-			]
-		);
-		$job->start();
-	}
-
-	/** Multiota specific. */
-
-	public function capitalizeJob()
-	{
-		$job = new \SeanMorris\Multiota\Job(
-			'SeanMorris\Multiota\Test\Capitalize\CapitalizeProcessor'
-			, 'SeanMorris\Multiota\RemotePool'
-			, [
-				'servers' => ['thewhtrbt.com', 'buzzingbeesalon.com']
-			]
-		);
-		$job->start();
-	}
-
-	/** Multiota specific. */
-
-	public function letterCountMap()
-	{
-		$job = new \SeanMorris\Multiota\Job(
-			'SeanMorris\Multiota\Test\LetterCount\Mapper'
-			//, 'SeanMorris\Multiota\RemotePool'
-			, [
-				'servers' => ['seantop', 'localhost']
-			]
-		);
-		$job->start();
-	}
-
-	/** Multiota specific. */
-
-	public function letterCountReduce()
-	{
-		$job = new \SeanMorris\Multiota\Job(
-			'SeanMorris\Multiota\Test\LetterCount\Reducer'
-			//, 'SeanMorris\Multiota\RemotePool'
-			, [
-				'servers' => ['seantop', 'localhost']
-			]
-		);
-		$job->start();
-	}
-
 	/** Start a RePL. */
 
 	public function repl()
@@ -1039,10 +952,12 @@ EOF
 			{
 				continue;
 			}
+
 			if($offset < 0)
 			{
 				$offset = 0;
 			}
+
 			if($offset > strlen($line))
 			{
 				$offset = strlen($line);
