cc04d08cdd01ff1452e76233ec468516159e032f
Sean Morris <sean@seanmorr.is>
2019-12-31 21:28:06 -0500

    yml file typo.

--- infra/apt/proxy-detect.sh
+++ infra/apt/proxy-detect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 test ! -z "$IDS_APT_PROXY_HOST$IDS_APT_PROXY_PORT"                         \
-&& timeout 1 bash -c <"/dev/tcp/$IDS_APT_PROXY_HOST"/"$IDS_APT_PROXY_PORT" \
+&& timeout 1 bash -c "</dev/tcp/$IDS_APT_PROXY_HOST"/"$IDS_APT_PROXY_PORT" \
 && echo "http://$IDS_APT_PROXY_HOST:$IDS_APT_PROXY_PORT"                   \
 || echo DIRECT;
