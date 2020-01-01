3e9f327a0239c99b0dbfe61ac6e8b14c66bacfdd
Sean Morris <sean@seanmorr.is>
2019-12-31 22:15:32 -0500

    Permissions setup

--- infra/apt/proxy-detect.sh
+++ infra/apt/proxy-detect.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-test ! -z $IDS_APT_PROXY_HOST$IDS_APT_PROXY_PORT                         \
+test ! -z "$IDS_APT_PROXY_HOST$IDS_APT_PROXY_PORT"                       \
 && timeout 1 bash -c "</dev/tcp/$IDS_APT_PROXY_HOST/$IDS_APT_PROXY_PORT" \
-&& echo -n http://$IDS_APT_PROXY_HOST:$IDS_APT_PROXY_PO                  \
-&& exit 0     \
-|| >2& echo -n DIRECT
-
+&& echo "PROXY http://$IDS_APT_PROXY_HOST:$IDS_APT_PROXY_PORT;"       \
+|| echo "DIRECT";
