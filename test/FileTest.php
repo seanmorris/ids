ffb11ec58837a8df4dd21621ef70a7af40452d5d
Sean Morris <sean@seanmorr.is>
2020-01-05 15:58:34 -0500

    More test coverage for  File.php

--- test/FileTest.php
+++ test/FileTest.php
@@ -8,70 +8,110 @@ class FileTest extends \UnitTestCase
 		$this->globalDir = $this->package->globalDir();
 		$this->localDir = $this->package->localDir();
 		$this->testData = "abcdefghijklmnop";
-		$this->testFilename = $this->localDir . 'testFile.txt';
-		
+		$this->testFilename = 'testFile.txt';
+		$this->testFilepath = $this->localDir . $this->testFilename;
+
 	}
 
 	public function testWrite()
 	{
 		$appendData = "12345678";
 
-		$file = new \SeanMorris\Ids\Disk\File($this->testFilename);
+		$file = new \SeanMorris\Ids\Disk\File($this->testFilepath);
 
 		$file->write($this->testData, FALSE);
 		$this->assertEqual(
 			$file->slurp()
 			, $this->testData
-			, 'Write failed for ' . $this->testFilename
+			, 'Write failed for ' . $this->testFilepath
 		);
 
 		$file->write($appendData);
 		$this->assertEqual(
 			$file->slurp()
 			, $this->testData . $appendData
-			, 'Append failed for ' . $this->testFilename
+			, 'Append failed for ' . $this->testFilepath
 		);
 
 		$file->write($this->testData, FALSE);
 		$this->assertEqual(
 			$file->slurp()
 			, $this->testData
-			, 'Truncate failed for ' . $this->testFilename
+			, 'Truncate failed for ' . $this->testFilepath
 		);
 	}
 
 	public function testRead()
 	{
-		$file = new \SeanMorris\Ids\Disk\File($this->testFilename);
-		$data = str_split(file_get_contents($this->testFilename));
+		$file = new \SeanMorris\Ids\Disk\File($this->testFilepath);
+		$data = str_split(file_get_contents($this->testFilepath));
 
-		while(!$file->eof()) 
+		while(!$file->eof())
 		{
 			$byte = array_shift($data);
 			$testByte = $file->read(1);
 
-			$this->assertEqual($byte, $testByte, 'Read failed for ' . $this->testFilename);
+			$this->assertEqual($byte, $testByte, 'Read failed for ' . $this->testFilepath);
 		}
 
-		$data = str_split(file_get_contents($this->testFilename));
+		$data = str_split(file_get_contents($this->testFilepath));
 
 		$testByte = $file->read(0, TRUE);
 
-		while(!$file->eof()) 
+		while(!$file->eof())
 		{
 			$byte = array_shift($data);
 			$testByte = $file->read(1);
 
-			$this->assertEqual($byte, $testByte, 'Re-read failed for ' . $this->testFilename);
+			$this->assertEqual($byte, $testByte, 'Re-read failed for ' . $this->testFilepath);
 		}
+	}
+
+	public function testBasename()
+	{
+		$file = new \SeanMorris\Ids\Disk\File($this->testFilepath);
+
+		$this->assertEqual(
+			$file->basename()
+			, $this->testFilepath
+			, 'Basename failed for ' . $this->testFilename
+		);
 	}
 
 	public function testDelete()
 	{
-		$file = new \SeanMorris\Ids\Disk\File($this->testFilename);
+		$file = new \SeanMorris\Ids\Disk\File($this->testFilepath);
 
 		$file->delete();
 
-		$this->assertFalse(file_exists($this->testFilename), 'Delete failed for ' . $this->testFilename);
+		$this->assertFalse(file_exists($this->testFilepath), 'Delete failed for ' . $this->testFilepath);
+	}
+
+	public function testAge()
+	{
+		$file = new \SeanMorris\Ids\Disk\File($this->testFilepath);
+
+		if($file->check())
+		{
+			$file->delete();
+		}
+
+		$file->write();
+
+		sleep(5);
+
+		$this->assertTrue(
+			$file->age() >= 5
+			, 'Age failed for ' . $this->testFilepath
+		);
+	}
+
+	public function testSubtract()
+	{
+		$file = new \SeanMorris\Ids\Disk\File($this->testFilepath);
+		$dir  = dirname($this->testFilepath);
+
+
+		var_dump($file->subtract($dir));
 	}
 }
