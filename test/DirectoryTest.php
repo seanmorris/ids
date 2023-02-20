96b47721398e42fad23cf6db1366ab1a9ae6dbc4
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-02-20 06:00:46 -0500

    PHP8.2

--- test/DirectoryTest.php
+++ test/DirectoryTest.php
@@ -2,6 +2,8 @@
 namespace SeanMorris\Ids\Test;
 class DirectoryTest extends \UnitTestCase
 {
+	protected $package, $globalDir, $localDir, $testFilename;
+
 	public function setUp()
 	{
 		$this->package = \SeanMorris\Ids\Package::get('SeanMorris\Ids');
